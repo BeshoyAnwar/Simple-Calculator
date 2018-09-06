070b5af4c9284f71f14d4f9f60db286746002f7c
Beshoy Anwar <32778960+BeshoyAnwar@users.noreply.github.com>
2018-09-06 04:12:06 +0200

    Update stack.h

--- source-code/stack.h
+++ source-code/stack.h
@@ -25,50 +25,50 @@ typedef struct {
  
  
 /*******************************************************************************
- *function name: createStack												   *
+ *function name: createStack						       *
  *function description: create stack structure with given multiples of chars   *
  *function inputs: size=>number of chars in stack                              *
- *function outputs: pointer to the stack that has been created				   *
+ *function outputs: pointer to the stack that has been created		       *
  *******************************************************************************/
 sstack* createStack(unsigned int size);
 
 /*******************************************************************************
- *function name: deleteStack												   *
+ *function name: deleteStack						       *
  *function description: destroy the memory allocated for given stack pointer   *
  *function inputs: stack=>pointer to the needing destroyed stack               *
- *function outputs: none				  									   *
+ *function outputs: none				  		       *
  *******************************************************************************/
 void deleteStack(sstack* stack);
 
 /*******************************************************************************
- *function name: push												  		   *
- *function description: add a new element on the top of the stack   		   *
+ *function name: push							       *
+ *function description: add a new element on the top of the stack   	       *
  *function inputs: stack=>pointer to the needing stack ,data: the new element  *
- *function outputs: none				  									   *
+ *function outputs: none				 		       *
  *******************************************************************************/
 void push(sstack* stack,char data);
 
 /*******************************************************************************
- *function name: pop												  		   *
- *function description: remove the top element of the stack   				   *
- *function inputs: stack=>pointer to the needing stack           			   *
- *function outputs: char carry the removed element data				  		   *
+ *function name: pop							       *
+ *function description: remove the top element of the stack   		       *
+ *function inputs: stack=>pointer to the needing stack           	       *
+ *function outputs: char carry the removed element data			       *
  *******************************************************************************/
 char pop(sstack* stack);
 
 /*******************************************************************************
- *function name: top												  		   *
+ *function name: top						               *
  *function description: return the top element of the stack with no remove     *
- *function inputs: stack=>pointer to the needing stack           			   *
- *function outputs: char carry the top element data				  		   	   *
+ *function inputs: stack=>pointer to the needing stack                         *
+ *function outputs: char carry the top element data			       *
  *******************************************************************************/
 char top(sstack* stack);
 
 /*******************************************************************************
- *function name: isEmpty												  	   *
- *function description: determine if the stack is empty or not   			   *
- *function inputs: stack=>pointer to the needing stack           			   *
- *function outputs: 1 if empty,0 if not empty				  		  		   *
+ *function name: isEmpty						       *
+ *function description: determine if the stack is empty or not   	       *
+ *function inputs: stack=>pointer to the needing stack           	       *
+ *function outputs: 1 if empty,0 if not empty		  		       *
  *******************************************************************************/
 unsigned char isEmpty(sstack* stack);
 
