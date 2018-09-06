043f31db72ee6f00f7faf3e0c0a682a9880d9cf4
Beshoy Anwar <32778960+BeshoyAnwar@users.noreply.github.com>
2018-09-06 04:17:17 +0200

    Update calculator.h

--- source-code/calculator.h
+++ source-code/calculator.h
@@ -15,59 +15,59 @@
 #include <string.h>
 
 /*************************************************************************************
- *                      Functions Prototypes                               		     *
+ *                      Functions Prototypes                                         *
  *************************************************************************************/
 /*************************************************************************************
- *function name: isOperand												             *
+ *function name: isOperand						             *
  *function description: private function to determine if the char is a number or not *
- *function inputs: data=>the char needed to check                              		 *
- *function outputs: 1 if the char is a number,otherwise 0	 				         *
+ *function inputs: data=>the char needed to check                              	     *
+ *function outputs: 1 if the char is a number,otherwise 0	 	             *
  *************************************************************************************/
 unsigned char static isOperand(char data);
 
 /*************************************************************************************
- *function name: priority												             *
- *function description: private function return the precedence of the operator		 *
+ *function name: priority							     *
+ *function description: private function return the precedence of the operator	     *
  *function inputs: operator=>the char needed to know its priority                    *
- *function outputs: 0 if +,- 	1 if *,/	otherwise -1 for error checking	 		 *
+ *function outputs: 0 if +,- 	1 if *,/	otherwise -1 for error checking	     *
  *************************************************************************************/
 char static priority(char operator);
 
 /*************************************************************************************
- *function name: infixTOPostfix												         *
+ *function name: infixTOPostfix			                                     *
  *function description: private function convert the the infix notation (3+5) to
-						post fix notation (35+)                                      *
+			post fix notation (35+)                                      *
  *function inputs: inExpression=>pointer to char have the has address carry
-							   infix expression                                      *
+		   infix expression                                                  *
  *function outputs: outExpression=>pointer to char have the has address carry
-								postfix expression 
-					unsigned char for error checking if no error return 1 
-													 else return 0					 *
+		    postfix expression 
+		    unsigned char for error checking if no error return 1 
+						     else return 0		     *
  *************************************************************************************/
 unsigned char static infixTOPostfix(char* inExpression,char* outExpression);
 
 /*************************************************************************************
- *function name: evaluatePostfix												     *     
+ *function name: evaluatePostfix	                                             *     
  *function description: private function evalute the postfix expression using stack  *
  *function inputs: expression=>pointer to char have the has address carry
-							   postfix expression                                    *
+		   postfix expression                                                *
  *function outputs: result=>pointer to char have the has address carry result of the
-					expression														  
-					unsigned char for error checking if no error return 1 
-													 else return 0					 *
+		    expression														  
+		    unsigned char for error checking if no error return 1 
+					             else return 0		     *
  *************************************************************************************/
 unsigned char static evaluatePostfix(char* expression,char* result);
 
 /*************************************************************************************
- *function name: evaluateExpression												     *     
+ *function name: evaluateExpression	                                             *     
  *function description: public function evalute the infix expression with help of
-						functions as infixTOPostfix,evaluatePostfix					 *
+			functions as infixTOPostfix,evaluatePostfix                  *
  *function inputs: expression=>pointer to char have the has address carry
-							   infix expression                                      *
- *function outputs: result=>pointer to char have the has address carry result of the
-					expression														  
-					unsigned char for error checking if no error return 1 
-													 else return 0					 *
+		   infix expression                                                  *
+ *function outputs: result=>pointer to char have the has address carry result of 
+ 		    the expression														  
+		    unsigned char for error checking if no error return 1 
+						     else return 0		     *
  *************************************************************************************/
 unsigned char evaluateExpression(char* expression,char* result);
 
